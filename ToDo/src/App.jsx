dbbd3474ea1635ce3c82e0e4f580cd933dae591f
Kelvin Zheng <kelvinzheng21@gmail.com>
2024-12-20 12:45:23 -0600

    added ability to remove items(just strings not dicts) from list by checking them

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -18,11 +18,21 @@ function App() {
     localStorage.setItem('items', JSON.stringify(items));
   }, [items])
 
+  const removeItem = (item) => {
+    console.log("removing " + item); // for debugging
+    let array = items;
+    let index = array.indexOf(item);
+    if (index != -1) {
+      array.splice(index, 1);
+    }
+    setItems(array);
+  }
+
   return (
     <div>
       <p>Testing</p>
-      <Item id={1} name={"Buy Groceries"} />
-      <Item id={2} name={"Take Out Trash"} />
+      <Item id={1} name={"Buy Groceries"} removeItem={removeItem}/>
+      <Item id={2} name={"Take Out Trash"} removeItem={removeItem}/>
     </div>
   )
 }
--- ToDo/src/Item.jsx
+++ ToDo/src/Item.jsx
@@ -1,8 +1,14 @@
-function Item({id, name}) {
+function Item({id, name, removeItem}) {
+  const handleChange = (event) => {
+    if (event.target.checked) {
+      removeItem(name);
+    }
+  }
+
   return (
     <div className="todo-item">
-      <input type="checkbox" id={id} value={name} />
-      <label for={id}> {name} </label>
+      <input type="checkbox" id={id} value={name} onChange={handleChange}/>
+      <label htmlFor={id}> {name} </label>
     </div>
   );
 };
